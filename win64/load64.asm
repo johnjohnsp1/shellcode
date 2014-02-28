a974dca4de228773dccc13aea1cecd462931d269
cmpxchg8 <wyse101@gmail.com>
2014-02-28 05:00:59 +0000

    Update load64.asm

--- win64/load64.asm
+++ win64/load64.asm
@@ -34,28 +34,26 @@ entrypoint:
     push   rsi
     push   rdi
     push   rbp
-    mov    r15, rsp
-    and    rsp, -16
-    sub    rsp, 28h
+    sub    rsp, 48h
     jmp    init_param
 calc_pos:
     push   60h
     pop    rcx
     mov    rsi, [gs:rcx]
-    mov    esi, [rsi+18h]
-    mov    esi, [rsi+10h]              ; InLoadOrderModuleList
-    lodsd                              ; skip ntdll.dll
-    mov    esi, [rax]                  ; kernel32.dll
-    mov    edi, [rsi+30h]              ; LDR_DATA_TABLE_ENTRY.DllBase    
-    add    ecx, dword ptr[rdi+3ch]
-    mov    ebx, dword ptr[rdi+rcx+28h]
+    mov    rsi, [rsi+18h]
+    mov    rsi, [rsi+10h]
+    lodsq
+    mov    rsi, [rax]
+    mov    rdi, [rsi+30h] 
+    add    ecx, [rdi+3ch]
+    mov    ebx, [rdi+rcx+28h]
     
-    mov    esi, dword ptr[rdi+rbx+20h]
-    add    esi, edi
+    mov    esi, [rdi+rbx+20h]
+    add    rsi, rdi
     
-    mov    ecx, dword ptr[rdi+rbx+24h]
-    add    ecx, edi
-    cdq
+    mov    ecx, [rdi+rbx+24h]
+    add    rcx, rdi
+    xor    edx, edx
 find_loop:
     movzx  ebp, word ptr[rcx+2*rdx]
     inc    edx
@@ -65,12 +63,13 @@ find_loop:
     cmp    byte ptr[rdi+rax+0bh], 'A'
     jne    find_loop
     
-    mov    esi, dword ptr[rdi+rbx+1ch]
-    add    esi, edi
-    add    edi, [rsi+4*rbp]  
+    mov    esi, [rdi+rbx+1ch]
+    add    rsi, rdi
+    mov    eax, [rsi+4*rbp]
+    add    rax, rdi    
     pop    rcx
-    call   rdi
-    mov    rsp, r15
+    call   rax
+    add    rsp, 48h
     
     pop    rbp
     pop    rdi
