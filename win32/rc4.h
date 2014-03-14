f04c9bdf56706218a8b0a6e0fa7d9d327bd2fc8b
cmpxchg8 <wyse101@gmail.com>
2014-03-14 22:17:51 +0000

    Update rc4.h

--- win32/rc4.h
+++ win32/rc4.h
@@ -1,6 +1,6 @@
 
 /* Copyright (c) 2014 Kevin Devine */
-/* Size = 53 bytes */
+/* Size = 51 bytes */
 
 char rc4[] = {
   /* 0000 */  "\x8B\xC4"      /*  mov eax, esp                 */
@@ -18,19 +18,18 @@ char rc4[] = {
   /* 0014 */  "\xFE\xC0"      /*  inc al                       */
   /* 0016 */  "\x8A\x0C\x30"  /*  mov cl, byte ptr [eax+esi]   */
   /* 0019 */  "\x02\xD9"      /*  add bl, cl                   */
-  /* 001B */  "\x8A\x14\x33"  /*  mov dl, byte ptr [ebx+esi]   */
-  /* 001E */  "\x88\x0C\x33"  /*  mov byte ptr [ebx+esi], cl   */
-  /* 0021 */  "\x88\x14\x30"  /*  mov byte ptr [eax+esi], dl   */
-  /* 0024 */  "\x02\xCA"      /*  add cl, dl                   */
-  /* 0026 */  "\x8A\x14\x31"  /*  mov dl, byte ptr [ecx+esi]   */
-  /* 0029 */  "\x30\x17"      /*  xor byte ptr [edi], dl       */
-  /* 002B */  "\xAE"          /*  scasb                        */
-  /* 002C */  "\x4D"          /*  dec ebp                      */
-  /* 002D */  "\x75\xE5"      /*  jne 00000014h                */
-  /* 002F */  "\x5F"          /*  pop edi                      */
+  /* 001B */  "\x86\x0C\x33"  /*  xchg byte ptr [ebx+esi], cl  */
+  /* 001E */  "\x88\x0C\x30"  /*  mov byte ptr [eax+esi], cl   */
+  /* 0021 */  "\x02\x0C\x33"  /*  add cl, byte ptr [ebx+esi]   */
+  /* 0024 */  "\x8A\x14\x31"  /*  mov dl, byte ptr [ecx+esi]   */
+  /* 0027 */  "\x30\x17"      /*  xor byte ptr [edi], dl       */
+  /* 0029 */  "\xAE"          /*  scasb                        */
+  /* 002A */  "\x4D"          /*  dec ebp                      */
+  /* 002B */  "\x75\xE7"      /*  jne 00000014h                */
+  /* 002D */  "\x5F"          /*  pop edi                      */
+  /* 002E */  "\xAB"          /*  stosd                        */
+  /* 002F */  "\x93"          /*  xchg eax, ebx                */
   /* 0030 */  "\xAB"          /*  stosd                        */
-  /* 0031 */  "\x93"          /*  xchg eax, ebx                */
-  /* 0032 */  "\xAB"          /*  stosd                        */
-  /* 0033 */  "\x61"          /*  popad                        */
-  /* 0034 */  "\xC3"          /*  ret                          */
+  /* 0031 */  "\x61"          /*  popad                        */
+  /* 0032 */  "\xC3"          /*  ret                          */
 };
